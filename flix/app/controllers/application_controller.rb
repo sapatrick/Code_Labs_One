5fa2851c1d5e41fb23a289fab7f8f3e02de10b08
sapatrick <sa.patrick@yahoo.com>
2018-03-03 13:46:03 -0600

    Added Admin-only access to delete accounts and to create, edit and delete movies.

--- flix/app/controllers/application_controller.rb
+++ flix/app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   def require_signin
     unless current_user
+      session[:intended_url] = request.url
       redirect_to new_session_url, alert: "Please sign in first!"
     end
   end
@@ -15,5 +16,22 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+	def current_user?(user)
+	current_user == user
+	end
+
+	helper_method :current_user?
+
+def require_admin
+  unless current_user_admin?
+    redirect_to root_url, alert: "Unauthorized access!"
+  end
+end
+
+def current_user_admin?
+  current_user && current_user.admin?
+end
+
+helper_method :current_user_admin?
 
 end
--- flix/app/controllers/users_controller.rb
+++ flix/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  
   before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
 	def index
 		@users = User.all
@@ -25,11 +26,9 @@ def create
 end
 
 	def edit
-  		@user = User.find(params[:id])
 	end
 
 	def update
-  		@user = User.find(params[:id])
   		if @user.update(user_params)
    		 redirect_to @user, notice: "Account successfully updated!"
  		 else
@@ -37,12 +36,19 @@ end
   		end
 	end
 
-	def destroy
-  		@user = User.find(params[:id])
-  		@user.destroy
-      session[:user_id] = nil
-  		redirect_to root_url, alert: "Account successfully deleted!"
-	end
+# User can delete their own account
+#	def destroy
+#  		@user.destroy
+#      session[:user_id] = nil
+#  		redirect_to root_url, alert: "Account successfully deleted!"
+#	end
+
+# Only an Admin can delete user accounts.
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_url, alert: "Account successfully deleted!"
+  end
 
 private
 
@@ -51,4 +57,9 @@ def user_params
     permit(:username, :name, :email, :password, :password_confirmation)
 end
 
-end
+  def require_correct_user
+    @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
+    end
+  end
+
